6a5b4ad1cf39fc5a029c2dc88992602e50535c14
Sean Phelan <seanphelan97@gmail.com>
2018-09-04 14:22:33 -0600

    add notes endpoint

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const {id} = req.params;
+  const matchingItem = data.find(item => item.id === Number(id));
+  res.json(matchingItem);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
