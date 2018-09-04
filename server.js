72dcaea6e6171de4c70f42652c609e25ecd802a0
Sean Phelan <seanphelan97@gmail.com>
2018-09-04 13:58:12 -0600

    base server

--- server.js
+++ server.js
@@ -2,7 +2,14 @@
 
 // Load array of notes
 const data = require('./db/notes');
+const express = require('express');
 
-console.log('Hello Noteful!');
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+app.use(express.static('public'));
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
